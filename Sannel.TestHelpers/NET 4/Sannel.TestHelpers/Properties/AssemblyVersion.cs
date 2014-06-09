7f7bcdeb3d184180a652e156a72c19f51f0bc07a
Adam Holt <adam.holt@manageddigital.com>
2014-06-09 09:36:05 -0600

    Moved Sannel.TestHelpers into its own Project.

--- Sannel.TestHelpers/NET 4/Sannel.TestHelpers/Properties/AssemblyVersion.cs
+++ Sannel.TestHelpers/NET 4/Sannel.TestHelpers/Properties/AssemblyVersion.cs
@@ -27,5 +27,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Sannel Software, LLC")]
 [assembly: AssemblyCopyright("Copyright © Sannel Software, L.L.C. 2014")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
